7557268a019c3eb0018d4784b95edeca83e70423
Jorge Nario <jorgienario@gmail.com>
2013-04-13 15:10:37 -0400

    Made a change

--- src/main/java/com/mycompany/mavenproject1/App.java
+++ src/main/java/com/mycompany/mavenproject1/App.java
@@ -6,6 +6,7 @@ package com.mycompany.mavenproject1;
  */
 public class App {
   public static void main( String[] args ) {
-      System.out.println(Jorgie.getName());
+    // This is another comment.
+    System.out.println(Jorgie.getName());
   }
 }
